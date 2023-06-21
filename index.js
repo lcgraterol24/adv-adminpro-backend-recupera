9403b645d756ab678d4448c1f1e9f0ff1022e0d5
=Lorena Graterol <=lcgraterol@gmail.com>
2023-06-21 16:29:56 -0300

    adjunto ruta * / para cuando hacer refresh

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -29,6 +30,15 @@ app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/upload', require('./routes/uploads') );
 app.use( '/api/login', require('./routes/auth') );
 
+
+// //lo ultimo: cualquier ruta que no sea la anterior
+// app.get('*', (req, res) =>{
+//     res.sendFile( path.resolve(__dirname, 'public/index.html'))
+// })
+//lo anterior sirve en caso de que haya copiado la carpeta de dist/adminpro de mi proyecto angular 
+//para la carpeta public de aca del backend. Pero yo lo tengo el proyectos separados
+
+app.get('*', '/');
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT );
